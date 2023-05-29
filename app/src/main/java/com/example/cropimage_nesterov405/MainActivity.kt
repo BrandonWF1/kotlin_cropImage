5bd90b558f0510cdd7cb71c7afd3344456cc8568
BrandonWF1 <ice22333@gmail.com>
2023-05-29 14:12:22 +0700

    Development

--- app/src/main/java/com/example/cropimage_nesterov405/MainActivity.kt
+++ app/src/main/java/com/example/cropimage_nesterov405/MainActivity.kt
@@ -8,8 +8,9 @@ import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
+
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
@@ -19,11 +20,11 @@ import com.theartofdev.edmodo.cropper.CropImage
 
 @RequiresApi(Build.VERSION_CODES.M)
 class MainActivity : AppCompatActivity() {
-    private var userPic :ImageView? = null
-    private val storageRequest = 200
-    private var storagePermission:Array<String>? = null
-    private var click:TextView? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private var userPic :ImageView? = null //ссылка на ImageView в макете
+    private val storageRequest = 200 // целочисленный код, который будет использоваться при запросе разрешения
+    private var storagePermission:Array<String>? = null // массив строк, который содержит разрешение на доступ к хранилищу
+    private var click:Button? = null //ссылка на кнопку "Choose"
+    override fun onCreate(savedInstanceState: Bundle?) { //инициализация переменных
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         click = findViewById(R.id.btnChoose)
@@ -31,26 +32,29 @@ class MainActivity : AppCompatActivity() {
 
 
         storagePermission = arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        click!!.setOnClickListener {
-            showImagePicDialog()
+        click!!.setOnClickListener { //установка обработчика нажатия на кнопку
+            showImagePicDialog() // диалоговое окно для выбора источника изображения.
         }
 
 
     }
 
+    //фукнция модального окна
     private fun showImagePicDialog() {
         val builder = AlertDialog.Builder(this)
             .setTitle("Pick Image From")
-            .setPositiveButton("Gallery"){_,_->
+            .setPositiveButton("Gallery"){_,_-> //когда пользователь жмет кнопку - функция проверяет доступ
                 if (!checkStoragePermission()){
-                    requestStoragePermission()
+                    requestStoragePermission() //повторный вызов функции для запроса разрешения в случае отрицаптельного ответа
                 }else{
-                    pickFromGallery()
+                    pickFromGallery() //функция выбора из галереи при положительном ответе
                 }
 
             }
-        builder.create().show()
+        builder.create().show() //метод который создает диалоговое окно и отображает его на экране
     }
+
+    //функция проверки доступа к хранилищу, не забываем добавить permission в AndroidManifest
     private fun checkStoragePermission(): Boolean {
         return ContextCompat.checkSelfPermission(
             this,android.Manifest.permission.WRITE_EXTERNAL_STORAGE
@@ -58,28 +62,32 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    //функция непосредственного вызова галереи и обрезки для пользователя (из библиотеки Cropper)
     private fun pickFromGallery() {
-        CropImage.activity().start(this@MainActivity)
+        CropImage.activity().start(this@MainActivity) //запуск в контексте активности методом start()
     }
 
+    //функция запроса разрешения к хранилищу
     private fun requestStoragePermission() {
         requestPermissions(storagePermission!!,storageRequest)
     }
 
+
+    //Функция обратного вызова, обрабатывает результат запроса разрешения на доступ к хранилищу(при первом запуске)
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array < String>,
-        grantResults: IntArray
+        requestCode: Int, // целочисленный код, который был передан при запросе разрешения.
+        permissions: Array < String>, //массив строк, который содержит запрошенные разрешения
+        grantResults: IntArray //массив целых чисел, который содержит результаты запроса разрешения
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == storageRequest){
-            if (grantResults.isNotEmpty()) {
-                val writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+        if (requestCode == storageRequest){ //storageRequest = заданная нами ранее переменная
+            if (grantResults.isNotEmpty()) { //проверяем, если массив больше нуля
+                val writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED //проверка доступа
                 if (writeStorageAccepted){
-                    pickFromGallery()
+                    pickFromGallery() //функция для вызова галереи
                 }else{
                     Toast.makeText(this,
-                        "Please Enable Storage Permission",
+                        "Please Enable Storage Permission", //вывод ошибки
                         Toast.LENGTH_SHORT).show()
                 }
             }
@@ -90,11 +98,11 @@ class MainActivity : AppCompatActivity() {
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            val result: CropImage.ActivityResult = CropImage.getActivityResult(data)
-            if (resultCode == RESULT_OK) {
-                val resultUri: Uri = result.uri
-                Picasso.with(this).load(resultUri).into(userPic)
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) { //проверка совпадения с кодом запроса обрезки
+            val result: CropImage.ActivityResult = CropImage.getActivityResult(data) //получаем обрезанное изображение
+            if (resultCode == RESULT_OK) { //проверка, если изображение обрезано успешно
+                val resultUri: Uri = result.uri //обрезанное изображение в переменную
+                Picasso.with(this).load(resultUri).into(userPic) //отрисовка с помощью библиотеки Picasso (по URI и идентификатору userPic)
             }
         }
     }
